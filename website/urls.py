cdc774d30d5e13efdbe2cb7cbffb3bb4d40c2d35
Mehedi Hasan <mehedihtanvir@gmail.com>
2024-04-22 20:37:17 +0600

    G-suite Email Verification Added

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path 
 from . import views
+
 urlpatterns = [
     path('', views.home, name='home'),
     # path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
+    path('token/', views.token_sent, name='token'),
+    path('success/', views.success, name='success'),
+    path('verify/<auth_token>', views.verify, name='verify'),
+    path('error/', views.error_page, name='error'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.contrib import messages
+from .models import *
+import uuid
+from django.conf import settings
+from django.core.mail import send_mail
+
 
 def home(request):
     if request.method == 'POST':
@@ -20,3 +26,69 @@ def logout_user(request):
     logout(request)
     messages.success(request, "Logged out Succesfully!")
     return redirect('home')
+
+   
+def register_user(request):
+    if request.method=='POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        try:
+            if not (email.endswith("@g.bracu.ac.bd") or email.endswith("@bracu.ac.bd")):
+                messages.success(request, 'Must be a BRACU G-suite email address!')
+                return redirect('/register')
+            if User.objects.filter(username=username).first():
+                messages.success(request, 'Username is taken!')
+                return redirect('/register')
+            
+            if User.objects.filter(email=email).first():
+                messages.success(request, 'There already exists an account with this email!')
+                return redirect('/register')
+            
+            user_obj = User(username=username, email=email)
+            user_obj.set_password(password)
+            user_obj.save()
+            auth_token = str(uuid.uuid4())
+            profile_obj = Profile.objects.create(user = user_obj, auth_token = auth_token)
+            profile_obj.save()
+            send_mail_after_registration(email, auth_token)
+            return redirect('/token')
+
+        except Exception as e:
+            print(e)
+
+    return render(request, 'register.html')
+
+def success(request):
+    return render(request, 'success.html')
+
+def token_sent(request):
+    return render(request, 'token_sent.html')
+
+def verify(request, auth_token):
+    try:
+        profile_obj = Profile.objects.filter(auth_token = auth_token).first()
+        if profile_obj:
+            if profile_obj.is_verified:
+                messages.success(request, 'Your account is already verified!')
+                return redirect('/')
+            profile_obj.is_verified=True
+            profile_obj.save()
+            messages.success(request, 'Your account has been verified successfully.')
+            return redirect('/')
+        else:
+            return redirect('/error')
+    except Exception as e:
+        print(e)
+
+def error_page(request):
+    return render(request, 'error_page.html')
+
+
+def send_mail_after_registration(email, token):
+    subject = "Your account needs to be verified"
+    message = f'Hi, follow this link to verify your STuber account http://127.0.0.1:8000/verify/{token}'
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = [email]
+    send_mail(subject, message, email_from, recipient_list)
